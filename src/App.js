93fe86f96fd774131a0f5a0f86040770872c1c22
Danny Lekim <dannylekim@Dannys-MBP.local>
2017-12-29 23:17:23 -0500

    starting http tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
+
+    let token;
+    axios.post('http://localhost:4000/signup',{
+      email: 'admin@mail.ca',
+      username: 'admin2',
+      firstName: "d",
+      lastName: "N",
+      password: "Abc123.",
+      role: "user"
+  }).then(function(data){
+      console.log(data)
+    }).catch(function (error){
+      console.log(error)
+    })
+   
+
     return (
       <div className="App">
         <header className="App-header">
@@ -12,6 +29,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        {token}
+        
         </p>
       </div>
     );
