307579a4ef03f639d78eab32a7b4951c0cdd21ad
Danny Le Kim <dannylekim@gmail.com>
2018-01-10 23:58:33 -0500

    clean signup form and particles.js

--- src/views/loginForm/loginForm.jsx
+++ src/views/loginForm/loginForm.jsx
@@ -4,7 +4,6 @@ import {
   Form,
   Grid,
   Header,
-  Message,
   Segment,
   Divider,
   Icon
@@ -12,6 +11,7 @@ import {
 import "./loginForm.css";
 import kantaskyUser from "../../api/userApi";
 import { Link } from "react-router-dom";
+import Particles from "react-particles-js";
 
 class loginForm extends React.Component {
   constructor() {
@@ -71,56 +71,168 @@ class loginForm extends React.Component {
 
   render() {
     const { error } = this.state;
-    return (
-      <div className="login-form">
-        <Grid
-          textAlign="center"
-          style={{ height: "100%" }}
-          verticalAlign="middle"
-        >
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as="h1" color="blue" textAlign="center">
-              Kantasky
-            </Header>
 
-            {this.state.error && (
-              <Segment inverted color="red" tertiary>
-                <Icon name="warning" />
-                {error}
+    const particles = {
+      particles: {
+        number: {
+          value: 99,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        },
+        color: {
+          value: "#ffffff"
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000"
+          },
+          polygon: {
+            nb_sides: 5
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: true,
+          anim: {
+            enable: true,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: true,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: true,
+          straight: false,
+          out_mode: "out",
+          bounce: true,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: true,
+            mode: "repulse"
+          },
+          onclick: {
+            enable: true,
+            mode: "push"
+          },
+          resize: true
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
+    };
+
+    return (
+      <div>
+        <Particles className="particle" params={particles} />
+        <div className="login-form">
+          <Grid
+            textAlign="center"
+            style={{ height: "100%" }}
+            verticalAlign="middle"
+          >
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Segment color="blue" tertiary>
+                <Header as="h1" color="blue" textAlign="center">
+                  Kantasky
+                </Header>
               </Segment>
-            )}
-            <Form size="large" onSubmit={this.handleSubmit}>
-              <Segment stacked secondary>
-                <Form.Input
-                  fluid
-                  icon="user"
-                  name="username"
-                  iconPosition="left"
-                  placeholder="Username or E-mail"
-                  onChange={this.handleInputChange}
-                />
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  name="password"
-                  iconPosition="left"
-                  placeholder="Password"
-                  type="password"
-                  onChange={this.handleInputChange}
-                />
-                <Button primary fluid type="submit">
-                  Login
-                </Button>
-                <Divider horizontal>Or</Divider>
-                <Link to="/signup">
-                  <Button secondary fluid>
-                    Sign Up Now
+
+              {this.state.error && (
+                <Segment inverted color="red" tertiary>
+                  <Icon name="warning" />
+                  {error}
+                </Segment>
+              )}
+              <Form size="large" onSubmit={this.handleSubmit}>
+                <Segment stacked secondary>
+                  <Form.Input
+                    fluid
+                    icon="user"
+                    name="username"
+                    iconPosition="left"
+                    placeholder="Username or E-mail"
+                    onChange={this.handleInputChange}
+                  />
+                  <Form.Input
+                    fluid
+                    icon="lock"
+                    name="password"
+                    iconPosition="left"
+                    placeholder="Password"
+                    type="password"
+                    onChange={this.handleInputChange}
+                  />
+                  <Button primary fluid type="submit">
+                    Login
                   </Button>
-                </Link>
-              </Segment>
-            </Form>
-          </Grid.Column>
-        </Grid>
+                  <Divider horizontal>Or</Divider>
+                  <Link to="/signup">
+                    <Button secondary fluid>
+                      Sign Up Now
+                    </Button>
+                  </Link>
+                </Segment>
+              </Form>
+            </Grid.Column>
+          </Grid>
+        </div>
       </div>
     );
   }
--- src/views/signUpForm/signUpForm.jsx
+++ src/views/signUpForm/signUpForm.jsx
@@ -10,6 +10,7 @@ import {
 import kantaskyUser from "../../api/userApi";
 import "./signUpForm.css";
 import { Link } from "react-router-dom";
+import Particles from "react-particles-js";
 
 class signupForm extends React.Component {
   constructor() {
@@ -25,14 +26,8 @@ class signupForm extends React.Component {
     };
 
     this.signUp = this.signUp.bind(this);
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(
-      this
-    );
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
-    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+
   }
 
   async signUp(event) {
@@ -44,100 +39,200 @@ class signupForm extends React.Component {
     }
   }
 
-  //setters
-  handleUsernameChange(e) {
-    this.setState({ username: e.target.value });
-  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-  handlePasswordChange(e) {
-    this.setState({ password: e.target.value });
-  }
-
-  handleConfirmPasswordChange(e) {
-    this.setState({ confirmPassword: e.target.value });
-  }
-
-  handleFirstNameChange(e) {
-    this.setState({ firstName: e.target.value });
-  }
-
-  handleLastNameChange(e) {
-    this.setState({ lastName: e.target.value });
-  }
-
-  handleEmailChange(e) {
-    this.setState({ email: e.target.value });
+    //set the appropriate value to the state
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
+    const particles = {
+      particles: {
+        number: {
+          value: 99,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        },
+        color: {
+          value: "#ffffff"
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000"
+          },
+          polygon: {
+            nb_sides: 5
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: true,
+          anim: {
+            enable: true,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: true,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: true,
+          straight: false,
+          out_mode: "out",
+          bounce: true,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: true,
+            mode: "repulse"
+          },
+          onclick: {
+            enable: true,
+            mode: "push"
+          },
+          resize: true
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
+    };
     return (
-      <div className="signup-form">
-        <Grid
-          textAlign="center"
-          style={{ height: "100%" }}
-          verticalAlign="middle"
-        >
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Message>
-              <Header as="h1" color="blue" textAlign="center">
-                Create a Kantasky Account!
-              </Header>
-            </Message>
-            <Form size="large" onSubmit={this.signUp}>
-              <Segment stacked>
-                <Form.Input
-                  fluid
-                  placeholder="First Name"
-                  type="text"
-                  onChange={this.handleFirstNameChange}
-                  required
-                />
-                <Form.Input
-                  fluid
-                  placeholder="Last Name"
-                  type="text"
-                  onChange={this.handleLastNameChange}
-                  required
-                />
-                <Form.Input
-                  fluid
-                  placeholder="Email"
-                  type="text"
-                  onChange={this.handleEmailChange}
-                  required
-                />
-                <Form.Input
-                  fluid
-                  placeholder="Username"
-                  type="text"
-                  onChange={this.handleUsernameChange}
-                  required
-                />
-                <Form.Input
-                  fluid
-                  placeholder="Password"
-                  type="password"
-                  onChange={this.handlePasswordChange}
-                  required
-                />
-                <Form.Input
-                  fluid
-                  placeholder="Confirm Password"
-                  type="password"
-                  onChange={this.handleConfirmPasswordChange}
-                  required
-                />
-                <Button color="blue" fluid size="large" type="submit">
-                  Sign-up
-                </Button>
-              </Segment>
-            </Form>
-            <Message>
-              Already have an account?
-              <Link to="/login"> Login</Link>
-            </Message>
-          </Grid.Column>
-        </Grid>
+      <div>
+        <Particles className="particle" params={particles} />
+        <div className="signup-form">
+          <Grid
+            textAlign="center"
+            style={{ height: "100%" }}
+            verticalAlign="middle"
+          >
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Message>
+                <Header as="h1" color="blue" textAlign="center">
+                  Create a Kantasky Account!
+                </Header>
+              </Message>
+              <Form size="large" onSubmit={this.signUp}>
+                <Segment stacked>
+                  <Form.Input
+                    fluid
+                    placeholder="First Name"
+                    type="text"
+                    name="firstName"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    placeholder="Last Name"
+                    type="text"
+                    name="lastName"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    placeholder="Email"
+                    type="text"
+                    name="email"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    placeholder="Username"
+                    type="text"
+                    name="username"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    placeholder="Confirm Password"
+                    type="password"
+                    name="confirmPassword"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <Button color="blue" fluid size="large" type="submit">
+                    Sign-up
+                  </Button>
+                </Segment>
+              </Form>
+              <Message>
+                Already have an account?
+                <Link to="/login"> Login</Link>
+              </Message>
+            </Grid.Column>
+          </Grid>
+        </div>
       </div>
     );
   }
