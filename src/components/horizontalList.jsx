7516919c5cc08794c051cfb72d35e068f2577c0f
Danny Le Kim <dannylekim@gmail.com>
2018-06-25 12:34:26 -0400

    Change look for viewGroupModal to be similar to taskModal

--- src/components/horizontalList.jsx
+++ src/components/horizontalList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Icon, List } from "semantic-ui-react";
+import { Icon, List, Label } from "semantic-ui-react";
 
-const HorizontalList = ({ users }) => {
+const HorizontalList = ({ users, teamLeader }) => {
   let listOfUsers;
   if (users) {
     listOfUsers = users.map(item => (
@@ -9,7 +9,7 @@ const HorizontalList = ({ users }) => {
         <Icon name="user" />
         <List.Content>
           <List.Header>
-            {item.userId === "general" ? "General" : item.userName}
+            {item.userId === "general" ? "General" : item.userName} {teamLeader && teamLeader.leaderId === item.userId ? (<Label pointing='left' size='tiny' color='blue'> Team Leader </Label>) : ("")}
           </List.Header>
         </List.Content>
       </List.Item>
